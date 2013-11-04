ec48c325830892335dd0a64aea5eac401091a66e
Dustin Green <dustingreen987@gmail.com>
2013-11-04 10:54:02 -0600

    added tweet read

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,13 @@ class TweetsController < ApplicationController
 
   def index
     @posts = Tweet.order('id DESC')
+    @new_tweet = Tweet.new
   end
 
+  def create
+    @new_tweet = Tweet.new(params[:post])
+    if(@new_tweet.save)
+      redirect_to :action => :index
+    end
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,4 @@
 class Tweet < ActiveRecord::Base
   attr_accessible :text
 
-  validates :tweet, :presence => true
-  validates :tweet, :length => { :in => 1..140 }
 end
